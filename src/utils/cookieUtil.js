fa9a199016ae09fbf8d68dcafd6e78c0e91e2067
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-08 15:55:24 +0100

    fix reading cookies when none present

--- src/utils/cookieUtil.js
+++ src/utils/cookieUtil.js
@@ -1,6 +1,6 @@
 export const readFavouritesCookie = () => {
     const match = document.cookie.match(/favourite-shibe-dogs=([^;]*)/);
-    if (match.length > 0) {
+    if (match && match.length > 0) {
         return JSON.parse(match[1]);
     }
     return null;
