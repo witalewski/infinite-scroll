11c3a020bbe0e5fda434c946f76293faa3c7f83e
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-08 07:33:04 +0100

    add base url to router| |

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/infinite-scroll">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
