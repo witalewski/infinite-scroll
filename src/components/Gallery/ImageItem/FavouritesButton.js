2a5719a7832a3d8b4decf46a7b1ec3fb06b437eb
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-08 17:29:40 +0100

    remove unnecessary styles and attributes from fav btn

--- src/components/Gallery/ImageItem/FavouritesButton.js
+++ src/components/Gallery/ImageItem/FavouritesButton.js
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 
 const FavouritesButtonStyled = styled.button`
   position: absolute;
-
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -13,19 +12,15 @@ const FavouritesButtonStyled = styled.button`
   padding-top: 12px;
   font-size: 64px;
 
-  background: rgba(255, 255, 255, 0.8);
-  border: none;
-
   &.btn--hidden {
     opacity: 0;
   }
 `;
 
-export const FavouritesButton = ({ visible, isFavourite, onButtonClick }) => (
+export const FavouritesButton = ({ visible, isFavourite }) => (
   <FavouritesButtonStyled
     className={`btn btn-light ${!visible && 'btn--hidden'}`}
     type="button"
-
   >
     {isFavourite ? '💔' : '❤️'}
   </FavouritesButtonStyled>
