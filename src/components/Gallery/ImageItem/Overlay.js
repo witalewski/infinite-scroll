2d02a3387ffb9740551999d73124a02d97819d57
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-08 16:52:12 +0100

    remove primary button entirely when not displayed

--- src/components/Gallery/ImageItem/Overlay.js
+++ src/components/Gallery/ImageItem/Overlay.js
@@ -2,28 +2,24 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const OverlayStyled = styled.div`
-    position: absolute;
-    top: 50%;
-    width: 100%;
-    height: 40%;
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  height: 40%;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    opacity: 1;
+  opacity: 1;
 
-    background: rgba(255, 255, 255, 0.9);
+  background: rgba(255, 255, 255, 0.9);
 
-    transition: opacity 0.2s;
-    transform: translateY(-50%);
+  transition: opacity 0.2s;
+  transform: translateY(-50%);
 
   &.overlay--hidden {
     opacity: 0;
-  }
-
-  .primary-button--hidden {
-    display: none;
   }
 
   .message {
@@ -44,23 +40,25 @@ export const Overlay = ({
   undoAction,
 }) => (
   <OverlayStyled className={`overlay ${!mouseOver && 'overlay--hidden'}`}>
-    <button
-      type="button"
-      className={`btn btn-outline-primary primary-button ${message &&
-        'primary-button--hidden'}`}
-      onClick={isFavourite ? removeFromFavourites : addToFavourites}
-    >
-      {isFavourite ? '💔 Remove from Favourites' : '❤️ Add to Favourites'}
-    </button>
-    { message && (<div className="message">
-      <span>{message}</span>
+    {!message ? (
       <button
         type="button"
-        className="btn btn-secondary btn-sm"
-        onClick={undoAction}
+        className="btn btn-outline-primary"
+        onClick={isFavourite ? removeFromFavourites : addToFavourites}
       >
-        Undo
+        {isFavourite ? '💔 Remove from Favourites' : '❤️ Add to Favourites'}
       </button>
-    </div>)}
+    ) : (
+      <div className="message">
+        <span>{message}</span>
+        <button
+          type="button"
+          className="btn btn-secondary btn-sm"
+          onClick={undoAction}
+        >
+          Undo
+        </button>
+      </div>
+    )}
   </OverlayStyled>
 );
