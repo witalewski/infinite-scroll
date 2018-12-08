7a47981fefd3475470b3b088fbefcbba65b4e92c
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-08 11:44:45 +0100

    add transitions

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from '@emotion/styled';
 
-export const ImageItem = ({ image, onImageLoad }) =>
-  image.isPlaceholder ? (
-    <div className="image-placeholder" />
-  ) : (
-    <img
-      className="image"
-      alt="Shibe dog"
-      src={image.url}
-      decoding="async"
-      onLoad={onImageLoad}
-    />
-  );
+const ImageItemStyled = styled.div`
+
+
+  }
+  .image {
+    opacity: 0;
+  }
+
+  .image--loaded {
+    opacity: 1;
+    transition: opacity 0.5s;
+  }
+`;
+
+export class ImageItem extends Component {
+  onImageLoad({ target }) {
+    target.classList.add('image--loaded');
+    target.parentNode.parentNode.style.height = `${target.clientHeight}px`;
+  }
+
+  render() {
+    const { image } = this.props;
+    return (
+      <ImageItemStyled>
+        {image.isPlaceholder ? (
+          <div className="image-placeholder">
+          </div>
+        ) : (
+          <img
+            className="image"
+            alt="Shibe dog"
+            src={image.url}
+            decoding="async"
+            onLoad={this.onImageLoad}
+          />
+        )}
+      </ImageItemStyled>
+    );
+  }
+}
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
+import styled from '@emotion/styled';
 import { ImageItem } from './ImageItem';
 
-export class ImageList extends Component {
-  onImageLoad({ target }) {
-    target.parentNode.style.minHeight = 0;
+const ImageListStyled = styled.ul`
+  .image-list--item {
+    transition: height 0.2s;
   }
+`;
+export class ImageList extends Component {
   render() {
     const { list, columnWidth } = this.props;
     return (
-      <ul className="image-list">
+      <ImageListStyled className="image-list">
         {list.map((image, i) => (
           <li
             className="image-list--item"
             key={`${image.url || 'placeholder'}-${i}`}
             style={{
-              minHeight: columnWidth * image.placeholderHeightRatio,
+              height: columnWidth * image.placeholderHeightRatio,
             }}
           >
-            <ImageItem image={image} onImageLoad={this.onImageLoad} />
+            <ImageItem image={image} />
           </li>
         ))}
-      </ul>
+      </ImageListStyled>
     );
   }
 }
