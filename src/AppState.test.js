21ed99d4465c27872689a5d57799de26176a70a7
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-08 18:04:57 +0100

    add tests for app state and height generator

--- src/AppState.test.js
+++ src/AppState.test.js
@@ -1,4 +1,5 @@
 import { AppState } from './AppState';
+import { Set } from 'immutable';
 import mockImageURLs from './fixtures/imageURLs.json';
 
 describe('AppState', () => {
@@ -11,5 +12,24 @@ describe('AppState', () => {
   it('sets image urls', () => {
     appState.setImageURLs(mockImageURLs);
     expect(appState.imageURLs).toEqual(mockImageURLs);
+  });
+
+  it('sets favourites', () => {
+    const favouritesSet = new Set(mockImageURLs);
+    appState.setFavourites(favouritesSet);
+    expect(appState.favourites).toEqual(favouritesSet);
+  });
+
+  it('adds to favourites', () => {
+    const favourite = 'img.jpg';
+    appState.addToFavourites(favourite);
+    expect(appState.favourites.includes(favourite)).toBe(true);
+  });
+
+  it('removes from to favourites', () => {
+    const favourite = 'img.jpg';
+    appState.addToFavourites(favourite);
+    appState.removeFromFavourites(favourite);
+    expect(appState.favourites.includes(favourite)).toBe(false);
   });
 });
