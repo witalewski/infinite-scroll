a46faaccc30d5b0bcd4b9147479fe9e2597dd47e
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-09 09:20:43 +0100

    improve title styling

--- src/components/Gallery/GalleryTitle.js
+++ src/components/Gallery/GalleryTitle.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const GalleryTitleStyled = styled.div`
+.title {
+    margin: 36px 0 24px;
+    text-align: center;
+}
+`;
 
 export const GalleryTitle = ({ title }) => (
-  <div className="row">
+  <GalleryTitleStyled className="row">
     <div className="col">
-      <h1>{title}</h1>
+      <h1 className="title">{title}</h1>
     </div>
-  </div>
+  </GalleryTitleStyled>
 );
